1c4db0b024d9fd4ac323f735034b266aa19f9a31
Viktoriia <vkkishko@gmail.com>
2024-11-08 21:11:23 +0200

    draft

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,65 @@
-export const Pagination = () => {};
+import React from 'react';
+import { getNumbers } from '../../utils';
+import classNames from 'classnames';
+
+interface Props {
+  total: number;
+  perPage: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+}
+
+export const Pagination: React.FC<Props> = ({
+  total,
+  perPage,
+  currentPage,
+  onPageChange,
+}) => {
+  const pages = getNumbers(1, 9);
+  const handleArrows = (arrow: string) => {
+    arrow === '«' ? onPageChange(currentPage - 1) : onPageChange(currentPage + 1);
+  }
+
+  return (
+    <>
+      <ul className="pagination">
+        <li className={classNames("page-item", {'disabled' : currentPage === pages[0]})}>
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled="true"
+            onClick={() => handleArrows('«')}
+          >
+            «
+          </a>
+        </li>
+
+        {pages.map(page => (
+          <li className={classNames("page-link", {'active' : page === currentPage})}>
+            <a
+            data-cy="pageLink"
+            className="page-link"
+            href={`#${page}`}
+            onClick = {() => onPageChange(page)}
+            >
+              {page}
+            </a>
+          </li>
+        ))}
+
+        <li className={classNames("page-item", {'disabled' : currentPage === pages[pages.length - 1]})}>
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#next"
+            aria-disabled="false"
+            onClick={() => handleArrows('»')}
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
